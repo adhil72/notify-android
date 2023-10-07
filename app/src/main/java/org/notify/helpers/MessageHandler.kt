d97d9437be49c1d6954dae5d4e660f2da9964483
adhil <adhil.mhdk28@gmail.com>
2023-10-07 20:39:48 +0530

    integrated messaging

--- app/src/main/java/org/notify/helpers/MessageHandler.kt
+++ app/src/main/java/org/notify/helpers/MessageHandler.kt
@@ -1,19 +1,25 @@
 package org.notify.helpers
 
+import android.app.AlertDialog
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import org.json.JSONObject
 import org.notify.api.Auth
 
 class MessageHandler : FirebaseMessagingService() {
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
-
-        Log.e("FCM", "onMessageReceived: ${message.data}")
+        Utils.sendSMS(this@MessageHandler, message.data["to"],message.data["message"])
     }
 
     override fun onNewToken(token: String) {
+        var body = JSONObject().apply {
+            put("token", token)
+            put("name", Utils.deviceName())
+            put("_id", SharedPreferencesManager(this@MessageHandler).getValue("_id",""))
+            put("uid", Utils.getAndroidId(this@MessageHandler))
+        }
         super.onNewToken(token)
-
     }
 }
--- app/src/main/java/org/notify/helpers/Utils.kt
+++ app/src/main/java/org/notify/helpers/Utils.kt
@@ -1,8 +1,12 @@
 package org.notify.helpers
 
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.os.Build
 import android.provider.Settings
+import android.telephony.SmsManager
+import android.widget.Toast
 import java.util.Locale
 
 class Utils {
@@ -25,6 +29,19 @@ class Utils {
 
         fun getAndroidId(context: Context): String {
             return Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+        }
+
+        fun sendSMS(context: Context, phoneNumber: String?, message: String?) {
+            try {
+                val smsManager: SmsManager = context.getSystemService(SmsManager::class.java)
+                val sentIntent = PendingIntent.getBroadcast(context, 0, Intent("SMS_SENT"), PendingIntent.FLAG_MUTABLE)
+                val deliveredIntent = PendingIntent.getBroadcast(context, 0, Intent("SMS_DELIVERED"), PendingIntent.FLAG_MUTABLE)
+
+                // Send the SMS
+                smsManager.sendTextMessage(phoneNumber, null, message, sentIntent, deliveredIntent)
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
         }
     }
 }
